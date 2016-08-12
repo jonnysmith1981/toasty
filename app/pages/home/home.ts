1e5424aedad1deb59b3a2132bb7a6d57347a84ae
Jonathan <rosso@amsddfurey1.miscs.com>
2016-08-12 16:33:10 +0100

    The next Initial Commit

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,25 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {ToastController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private toastCtrl: ToastController) {
   
+  }
+
+  toastify(): void {
+    this.toastWrapper('Cor blimey guvnor');
+  }
+
+  private toastWrapper(msg: string) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
   }
 }
